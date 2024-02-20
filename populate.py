02a5744833cc3467bf888d04ddc01509d5774842
Jesungha <113495115+Jesungha@users.noreply.github.com>
2024-02-20 13:06:58 -0600

    Fixed feb first

--- populate.py
+++ populate.py
@@ -6,7 +6,7 @@ def printout():
     total_sales = 0
 
     i = 0
-    for month in range(1,13):
+    for month in range(2,13):
         if month in [1,3,5,7,8,10,12]:
             days = 31
         elif month == 2:
@@ -39,6 +39,27 @@ def printout():
                 payments = ["cash", "credit", "debit"]
                 f.write(payments[random.randrange(0,3)]+ "\n")
                 i += 1
+ 
+    for day in range(1,32):
+        day_sale = 0
+        if day == 17:
+            day_limit = 8000
+        else:
+            day_limit = 2750
+        while day_sale < day_limit:
+            f.write(str(i)+", "+ "2024-"+ str(1)+ "-" + str(day)+ ", ")
+            f.write(str(random.randrange(11,23))+ ":" + str(random.randrange(0,60))+ ":" + str(random.randrange(0,60))+ ", ") # 11am to pm
+            subtotal = random.randrange(5,30)
+            tax = subtotal * .0825
+            total = subtotal + tax
+            total_sales += subtotal
+            day_sale += subtotal
+            f.write(str(subtotal)+ ", ") # subtotal
+            f.write(str(format(tax, '.2f'))+ ", ")# tax
+            f.write(str(format(total, '.2f'))+ ", ")   # total
+            payments = ["cash", "credit", "debit"]
+            f.write(payments[random.randrange(0,3)]+ "\n")
+            i += 1
     print (total_sales)
     f.close()
 
